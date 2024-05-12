7e57a5ec863e502655b2d20b91a46592221ccddf
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2024-05-12 11:41:20 +0530

    fixed crash

--- TalkIn/app/src/main/java/com/example/talk_in/SignUp.kt
+++ TalkIn/app/src/main/java/com/example/talk_in/SignUp.kt
@@ -3,6 +3,7 @@ package com.example.talk_in
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -12,63 +13,72 @@ import com.google.firebase.database.FirebaseDatabase
 
 class SignUp : AppCompatActivity() {
 
-    private lateinit var edtName: EditText
-    private lateinit var edtEmail: EditText
-    private lateinit var edtPassword: EditText
+  private lateinit var edtName: EditText
+  private lateinit var edtEmail: EditText
+  private lateinit var edtPassword: EditText
 
-    private lateinit var btnSignUp: Button
-    private lateinit var mAuth: FirebaseAuth
-private lateinit var mDbRef: DatabaseReference
+  private lateinit var btnSignUp: Button
+  private lateinit var mAuth: FirebaseAuth
+  private lateinit var mDbRef: DatabaseReference
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_sign_up)
 
-        supportActionBar?.hide()
+    supportActionBar?.hide()
 
-        mAuth = FirebaseAuth.getInstance()
+    mAuth = FirebaseAuth.getInstance()
 
-        edtName = findViewById(R.id.edt_name)
-        edtEmail = findViewById(R.id.edt_email)
-        edtPassword = findViewById(R.id.edt_password)
+    edtName = findViewById(R.id.edt_name)
+    edtEmail = findViewById(R.id.edt_email)
+    edtPassword = findViewById(R.id.edt_password)
 
-        btnSignUp = findViewById(R.id.btnSignup)
+    btnSignUp = findViewById(R.id.btnSignup)
 
-        btnSignUp = findViewById(R.id.btnSignup)
-
-
-        btnSignUp.setOnClickListener{
-            val name = edtName.text.toString()
-            val email = edtEmail.text.toString()
-            val password = edtPassword.text.toString()
-
-            signUp(name,email,password)
-
-    }
-}
-    private fun signUp(name:String, email: String, password: String){
-        //logic of creating user
-        mAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    //for verification of email address
+    btnSignUp = findViewById(R.id.btnSignup)
 
 
-                    //code for jumping home activity
-                    addUserToDatabase(name,email,mAuth.currentUser?.uid!!)
-                    val intent= Intent(this@SignUp,MainActivity::class.java)
-                    finish()
-                    startActivity(intent)
-
-                } else {
-                    Toast.makeText(this@SignUp,"Please Try Again,Some Error Occurred",Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
-
-    private fun addUserToDatabase(name: String, email: String, uid: String){
-        mDbRef= FirebaseDatabase.getInstance().getReference()
-        mDbRef.child("user").child(uid).setValue(User(name,email,uid))
+    btnSignUp.setOnClickListener {
+      val name = edtName.text.toString()
+      val email = edtEmail.text.toString()
+      val password = edtPassword.text.toString()
+      if (name.isBlank() || email.isBlank() || password.isBlank()) {
+        Toast.makeText(this, "Please enter details.", Toast.LENGTH_SHORT).show()
+      } else
+        signUp(name, email, password)
 
     }
+  }
+
+  private fun signUp(name: String, email: String, password: String) {
+    //logic of creating user
+    try {
+      mAuth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this) { task ->
+          if (task.isSuccessful) {
+            //for verification of email address
+
+
+            //code for jumping home activity
+            addUserToDatabase(name, email, mAuth.currentUser?.uid!!)
+            val intent = Intent(this@SignUp, MainActivity::class.java)
+            finish()
+            startActivity(intent)
+
+          } else {
+            Toast.makeText(this@SignUp, "Please Try Again,Some Error Occurred", Toast.LENGTH_SHORT)
+              .show()
+          }
+        }
+    } catch (e: Exception) {
+      Log.e("#", e.message.toString())
+    }
+
+  }
+
+  private fun addUserToDatabase(name: String, email: String, uid: String) {
+    mDbRef = FirebaseDatabase.getInstance().getReference()
+    mDbRef.child("user").child(uid).setValue(User(name, email, uid))
+
+  }
 }
