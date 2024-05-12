b94c1558dcd15bfcc7fb6508913778b28fcec8f3
ommaniya70 <ommaniya01@gmail.com>
2024-05-12 13:49:40 +0530

    Transitions added

--- TalkIn/app/src/main/java/com/example/talk_in/EntryActivity.kt
+++ TalkIn/app/src/main/java/com/example/talk_in/EntryActivity.kt
@@ -9,7 +9,7 @@ import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
-class EntryActivity : AppCompatActivity() {
+class   EntryActivity : AppCompatActivity() {
     private lateinit var videoView: VideoView // Declare videoView as a property
     private lateinit var mAuth: FirebaseAuth
 
@@ -22,6 +22,7 @@ class EntryActivity : AppCompatActivity() {
             val intent = Intent(this@EntryActivity,MainActivity::class.java)
             finish()
             startActivity(intent)
+            overridePendingTransition(R.anim.slide_out_bottom, R.anim.slide_in_bottom);
         }
 
         //hide actionbar
@@ -39,12 +40,14 @@ class EntryActivity : AppCompatActivity() {
         signInBtn.setOnClickListener {
             val intent = Intent(this, LogIn::class.java)
             startActivity(intent)
+            overridePendingTransition(R.anim.slide_out_bottom, R.anim.slide_in_bottom);
         }
         //signup button
         val signUpBtn: Button = findViewById(R.id.signupBtn)
         signUpBtn.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
+            overridePendingTransition(R.anim.slide_out_bottom, R.anim.slide_in_bottom);
         }
     }
     override fun onPause() {
