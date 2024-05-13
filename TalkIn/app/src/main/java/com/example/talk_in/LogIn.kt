c8ccc842132c65e0cf7e88470b152a28fdc1a05b
Nitin <sigilipellinitin@gmail.com>
2024-05-13 15:29:46 +0530

    Dark Theme problem fixed and Login Functionality improved and user flow improved(backstack)

--- TalkIn/app/src/main/java/com/example/talk_in/LogIn.kt
+++ TalkIn/app/src/main/java/com/example/talk_in/LogIn.kt
@@ -1,26 +1,33 @@
 package com.example.talk_in
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.content.Intent
-import android.util.Log
+import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.play.integrity.internal.i
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 
 
 class LogIn : AppCompatActivity() {
   private lateinit var mAuth: FirebaseAuth
-
+  private lateinit var edtEmail : com.google.android.material.textfield.TextInputEditText
+  private lateinit var edtPassword : com.google.android.material.textfield.TextInputEditText
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_log_in)
     mAuth = FirebaseAuth.getInstance()
     supportActionBar?.hide()
 
-    val edtEmail = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.edt_email)
-    val edtPassword = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.edt_password)
+    edtEmail = findViewById(R.id.edt_email)
+    edtPassword = findViewById(R.id.edt_password)
     val btnLogIn = findViewById<Button>(R.id.btnLogin)
     val backbtn = findViewById<ImageView>(R.id.btnBack)
 
@@ -29,8 +36,9 @@ class LogIn : AppCompatActivity() {
       val password = edtPassword.text.toString()
       if (email.isBlank() || password.isBlank()) {
         Toast.makeText(this, "Please enter details.", Toast.LENGTH_SHORT).show()
-      } else
+      } else{
         login(email, password)
+      }
     }
     backbtn.setOnClickListener{
       val intent = Intent(this@LogIn,EntryActivity::class.java)
@@ -42,32 +50,37 @@ class LogIn : AppCompatActivity() {
 
   }
 
-  private fun login(email: String, password: String) {
-    //login for logging user
-
-    try {
-      mAuth.signInWithEmailAndPassword(email, password)
-        .addOnCompleteListener(this) { task ->
-          if (task.isSuccessful) {
-            // code for logging
-
-            val intent = Intent(this@LogIn, MainActivity::class.java)
-            finish()
-            startActivity(intent)
-
-
-          } else {
-            Toast.makeText(
-              this@LogIn,
-              "User doesn't exist..! Please Sign-Up..",
-              Toast.LENGTH_SHORT
-            ).show()
-          }
+    private fun login(email: String, pwd: String) {
+        // CHecking for empty texts
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pwd)) {
+            mAuth.signInWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(OnCompleteListener<AuthResult?> { task ->
+                    if (task.isSuccessful) {
+                        //isEmailVerified()
+                        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this@LogIn,MainActivity::class.java)
+                        startActivity(intent)
+                        finish();
+                    }
+                }).addOnFailureListener(OnFailureListener { e ->
+                    if (e is FirebaseAuthInvalidCredentialsException) {
+                        edtPassword.error = "Invalid Password"
+                        edtPassword.requestFocus()
+                    }
+                    if (e is FirebaseAuthInvalidUserException) {
+                        edtEmail.error = "Email Not Registered"
+                        edtEmail.requestFocus()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Something went Wrong",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                })
+        } else {
+            Toast.makeText(this, "Please Enter Email & Password", Toast.LENGTH_SHORT).show()
         }
-    } catch (e: Exception) {
-      Log.e("#", e.message.toString())
     }
-
-  }
 
 }
