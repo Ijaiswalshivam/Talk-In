13410b8d6fc2498573008c05c83440bd95986979
hdhimalay <hdhimalay@gmail.com>
2024-05-19 14:48:18 +0530

    Fcm Notification added

--- TalkIn/app/src/main/java/com/example/talk_in/MainActivity.kt
+++ TalkIn/app/src/main/java/com/example/talk_in/MainActivity.kt
@@ -60,17 +60,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.logout) {
-            //logic for logout
-            mAuth.signOut()
-            val intent = Intent(this@MainActivity,EntryActivity::class.java)
-            startActivity(intent)
-            finish()
-           // finish()
+            // Remove device token from Firebase database
+            val currentUser = mAuth.currentUser
+            currentUser?.uid?.let { userId ->
+                mDbRef.child("users-device-tokens").child(userId).removeValue()
+                    .addOnSuccessListener {
+                        mAuth.signOut()
+                        val intent = Intent(this@MainActivity, LogIn::class.java)
+                        finish()
+                        startActivity(intent)
+                    }
+                    .addOnFailureListener { e ->
+                    }
+            }
             return true
         }
-        return true
+        return super.onOptionsItemSelected(item)
     }
     override fun onBackPressed() {
+        super.onBackPressed()
         val intent = Intent(Intent.ACTION_MAIN)
         intent.addCategory(Intent.CATEGORY_HOME)
         startActivity(intent)
--- TalkIn/app/src/main/java/com/example/talk_in/SignUp.kt
+++ TalkIn/app/src/main/java/com/example/talk_in/SignUp.kt
@@ -12,7 +12,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.firestore.FirebaseFirestore
 
 class SignUp : AppCompatActivity() {
 
