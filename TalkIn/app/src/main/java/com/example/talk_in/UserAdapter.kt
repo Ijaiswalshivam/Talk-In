1e0c07e5b59c2d371a677ca789550fc54842cd57
hdhimalay <hdhimalay@gmail.com>
2024-06-02 13:16:47 +0530

    Notification and Notification reply decrypt is fixed & internet connection error

--- TalkIn/app/src/main/java/com/example/talk_in/UserAdapter.kt
+++ TalkIn/app/src/main/java/com/example/talk_in/UserAdapter.kt
@@ -9,13 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import java.io.File
@@ -61,14 +58,26 @@ class UserAdapter(val context: Context, val userList: ArrayList<User>) :
                 override fun onDataChange(snapshot: DataSnapshot) {
                     if (snapshot.exists()) {
                         val lastMessage = snapshot.children.first().getValue(Message::class.java)
-                        holder.txt_last_message.setText(lastMessage?.message.toString())
-                    }
-                    else{
+                        val decryptedMessage = try {
+                            if (isHex(lastMessage?.message.toString())) {
+                                AESUtils.decrypt(lastMessage?.message.toString())
+                            } else {
+                                "Invalid message format"
+                            }
+                        } catch (e: IllegalArgumentException) {
+                            Log.e("Decryption Error", "Failed to decrypt message: ${e.message}")
+                            "Invalid message"
+                        } catch (e: Exception) {
+                            Log.e("Decryption Error", "An error occurred during decryption: ${e.message}")
+                            "Error decrypting message"
+                        }
+                        holder.txt_last_message.text = decryptedMessage
+                    } else {
                         var aboutUser = currentUser.aboutMe.toString().trim()
-                        if (aboutUser.length > 30){
+                        if (aboutUser.length > 30) {
                             aboutUser = aboutUser.substring(0, minOf(currentUser.aboutMe.toString().length, 30)).trim() + "..."
                         }
-                        holder.txt_last_message.setText(aboutUser)
+                        holder.txt_last_message.text = aboutUser
                     }
                 }
 
@@ -83,25 +92,31 @@ class UserAdapter(val context: Context, val userList: ArrayList<User>) :
     }
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textName = itemView.findViewById<TextView>(R.id.txt_name)
-        val userprofileImage = itemView.findViewById<ImageView>(R.id.user_profile_image)
-        val txt_last_message = itemView.findViewById<TextView>(R.id.txt_last_message)
+        val textName: TextView = itemView.findViewById(R.id.txt_name)
+        val userprofileImage: ImageView = itemView.findViewById(R.id.user_profile_image)
+        val txt_last_message: TextView = itemView.findViewById(R.id.txt_last_message)
     }
 
     private fun setProfileImage(uid: String, holder: UserViewHolder) {
-        storageReference = FirebaseStorage.getInstance().reference.child("user_profile_images")
-            .child("$uid.jpg")
+        val storageReference = FirebaseStorage.getInstance().reference.child("user_profile_images").child("$uid.jpg")
+
         try {
             val localFile = File.createTempFile("tempfile", ".jpg")
             storageReference.getFile(localFile)
                 .addOnSuccessListener {
                     val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
-                    holder.userprofileImage.setImageBitmap(bitmap)
-                }.addOnFailureListener{
+                    val circularBitmapDrawable = RoundedBitmapDrawableFactory.create(holder.itemView.resources, bitmap)
+                    circularBitmapDrawable.isCircular = true
+                    holder.userprofileImage.setImageDrawable(circularBitmapDrawable)
+                }.addOnFailureListener {
                     holder.userprofileImage.setImageResource(R.drawable.user_profile_icon)
                 }
         } catch (e: IOException) {
             e.printStackTrace()
         }
+    }
+
+    private fun isHex(str: String): Boolean {
+        return str.all { it in '0'..'9' || it in 'a'..'f' || it in 'A'..'F' }
     }
 }
