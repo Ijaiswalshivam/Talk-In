975d0f366d02aa20a2ca66ab4f282dd19f993f87
hdhimalay <hdhimalay@gmail.com>
2024-06-02 13:32:47 +0530

    Revert "Reapply "Merge branch 'main' of https://github.com/hdhimalay/Talk-In""
    This reverts commit 138ed52cdaf8736c0c163d396c1720ea471fbe04.

--- TalkIn/app/src/main/java/com/example/talk_in/UserAdapter.kt
+++ TalkIn/app/src/main/java/com/example/talk_in/UserAdapter.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -62,7 +61,7 @@ class UserAdapter(val context: Context, val userList: ArrayList<User>) :
                 override fun onDataChange(snapshot: DataSnapshot) {
                     if (snapshot.exists()) {
                         val lastMessage = snapshot.children.first().getValue(Message::class.java)
-                        holder.txt_last_message.setText(AESUtils.decrypt(lastMessage?.message.toString()))
+                        holder.txt_last_message.setText(lastMessage?.message.toString())
                     }
                     else{
                         var aboutUser = currentUser.aboutMe.toString().trim()
@@ -90,16 +89,14 @@ class UserAdapter(val context: Context, val userList: ArrayList<User>) :
     }
 
     private fun setProfileImage(uid: String, holder: UserViewHolder) {
-        val storageReference = FirebaseStorage.getInstance().reference.child("user_profile_images").child("$uid.jpg")
-
+        storageReference = FirebaseStorage.getInstance().reference.child("user_profile_images")
+            .child("$uid.jpg")
         try {
             val localFile = File.createTempFile("tempfile", ".jpg")
             storageReference.getFile(localFile)
                 .addOnSuccessListener {
                     val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
-                    val circularBitmapDrawable = RoundedBitmapDrawableFactory.create(holder.itemView.resources, bitmap)
-                    circularBitmapDrawable.isCircular = true
-                    holder.userprofileImage.setImageDrawable(circularBitmapDrawable)
+                    holder.userprofileImage.setImageBitmap(bitmap)
                 }.addOnFailureListener{
                     holder.userprofileImage.setImageResource(R.drawable.user_profile_icon)
                 }
--- TalkIn/app/src/main/java/com/example/talk_in/UserProfileScreen.kt
+++ TalkIn/app/src/main/java/com/example/talk_in/UserProfileScreen.kt
@@ -2,21 +2,27 @@ package com.example.talk_in
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.example.talk_in.databinding.ActivityUserProfileScreenBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import java.io.File
+import java.io.IOException
+
 
 class UserProfileScreen : AppCompatActivity() {
 
@@ -32,11 +38,11 @@ class UserProfileScreen : AppCompatActivity() {
         binding = ActivityUserProfileScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Initialize Firebase Auth and Database Reference
         mAuth = FirebaseAuth.getInstance()
         mDbRef = FirebaseDatabase.getInstance().getReference()
 
         USER_MODE = intent.getStringExtra("MODE").toString()
+
         var currentUserUid = mAuth.currentUser?.uid.toString()
         if (USER_MODE == "RECEIVER_USER") {
             currentUserUid = intent.getStringExtra("RECEIVER_UID").toString()
@@ -50,7 +56,7 @@ class UserProfileScreen : AppCompatActivity() {
 
         setProfileImage(currentUserUid)
 
-        currentUserUid.let { uid ->
+        currentUserUid?.let { uid ->
             mDbRef.child("user").child(uid).get().addOnSuccessListener { snapshot ->
                 val currentUser = snapshot.getValue(User::class.java)
                 currentUser?.let {
@@ -58,11 +64,6 @@ class UserProfileScreen : AppCompatActivity() {
                     binding.emailid.text = it.email
                     binding.showLocationToggleBtn.isChecked = it.showLocation!!
                     binding.aboutMeTextView.text = it.aboutMe
-                    if (it.mobile.isNullOrEmpty()) {
-                        binding.phoneNumber.text = "Add a new Phone Number"
-                    } else {
-                        binding.phoneNumber.text = it.mobile
-                    }
                 }
             }.addOnFailureListener { exception ->
                 // Handle any potential errors here
@@ -70,15 +71,15 @@ class UserProfileScreen : AppCompatActivity() {
         }
 
         binding.showLocationToggleBtn.setOnCheckedChangeListener { _, isChecked ->
-            currentUserUid.let { uid ->
+            currentUserUid?.let { uid ->
                 mDbRef.child("user").child(uid).child("showLocation").setValue(isChecked)
             }
         }
 
         binding.userprofileImageBtn.setOnClickListener {
             val intent = Intent()
-            intent.type = "image/*"
-            intent.action = Intent.ACTION_GET_CONTENT
+            intent.setType("image/*")
+            intent.setAction(Intent.ACTION_GET_CONTENT)
             startActivityForResult(intent, 100)
         }
 
@@ -86,15 +87,14 @@ class UserProfileScreen : AppCompatActivity() {
             val currentUser = mAuth.currentUser
             currentUser?.uid?.let { userId ->
                 mDbRef.child("users-device-tokens").child(userId).removeValue()
-                    .addOnSuccessListener {
-                        mAuth.signOut()
-                        val intent = Intent(this@UserProfileScreen, LogIn::class.java)
-                        finish()
-                        startActivity(intent)
-                    }
-                    .addOnFailureListener { e ->
-                        // Handle error
-                    }
+                        .addOnSuccessListener {
+                            mAuth.signOut()
+                            val intent = Intent(this@UserProfileScreen, LogIn::class.java)
+                            finish()
+                            startActivity(intent)
+                        }
+                        .addOnFailureListener { e ->
+                        }
             }
         }
 
@@ -105,59 +105,34 @@ class UserProfileScreen : AppCompatActivity() {
         }
 
         binding.editAboutIcon.setOnClickListener {
+            // Call the function passing the activity context and a lambda for handling the result
             showCustomDialog(this) { newText ->
                 // Handle the result (newText) here
-                mAuth.currentUser?.let { it1 ->
-                    mDbRef.child("user").child(it1.uid).child("aboutMe").setValue(newText)
-                        .addOnSuccessListener {
-                            binding.aboutMeTextView.text = newText
-                        }
-                        .addOnFailureListener{
-                            Toast.makeText(this@UserProfileScreen, "Failed to update value!!", Toast.LENGTH_SHORT).show()
-                        }
-                }
-            }
-        }
-
-        binding.editContactIcon.setOnClickListener {
-            showPhoneNumberDialog(this) { newPhoneNumber ->
-                mAuth.currentUser?.let { currentUser ->
-                    val userUid = currentUser.uid
-                    mDbRef.child("user").child(userUid).child("mobile").setValue(newPhoneNumber)
-                        .addOnSuccessListener {
-                            binding.phoneNumber.text = newPhoneNumber
-                            Toast.makeText(this@UserProfileScreen, "Phone number updated successfully!", Toast.LENGTH_SHORT).show()
-                        }
-                        .addOnFailureListener {
-                            Toast.makeText(this@UserProfileScreen, "Failed to update phone number!", Toast.LENGTH_SHORT).show()
-                        }
-                }
+                Toast.makeText(this, "New text: $newText", Toast.LENGTH_SHORT).show()
             }
         }
 
     }
 
-    private fun showCustomDialog(context: Context, listener: (String) -> Unit) {
+    fun showCustomDialog(context: Context, listener: (String) -> Unit) {
         val dialogView = LayoutInflater.from(context).inflate(R.layout.custom_dialog_layout, null)
         val editText = dialogView.findViewById<EditText>(R.id.edit_text)
 
-        AlertDialog.Builder(context)
+        val dialog = AlertDialog.Builder(context)
             .setView(dialogView)
             .setTitle("Edit Status")
             .setPositiveButton("Save") { _, _ ->
-                val newText = editText.text.toString().trim()
-                if (newText.isNotEmpty()) {
-                    listener.invoke(newText)
-                } else {
-                    Toast.makeText(this, "Status cannot be empty.", Toast.LENGTH_SHORT).show()
-                }
+                val newText = editText.text.toString()
+                listener.invoke(newText)
             }
             .setNegativeButton("Cancel") { dialog, _ ->
                 dialog.cancel()
             }
             .create()
-            .show()
+
+        dialog.show()
     }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -188,43 +163,27 @@ class UserProfileScreen : AppCompatActivity() {
     private fun setProfileImage(currentUserUid: String) {
         storageReference = FirebaseStorage.getInstance().reference.child("user_profile_images")
             .child("$currentUserUid.jpg")
-        storageReference.downloadUrl.addOnSuccessListener { uri ->
-            Glide.with(this)
-                .load(uri)
-                .circleCrop()
-                .placeholder(R.drawable.default_profile_image)
-                .error(R.drawable.error_profile_image)
-                .into(binding.userprofileImage)
+        try {
+            val localFile = File.createTempFile("tempfile", ".jpg")
+            storageReference.getFile(localFile)
+                .addOnSuccessListener {
+                    val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+                    val circularBitmapDrawable =
+                        RoundedBitmapDrawableFactory.create(resources, bitmap)
+                    circularBitmapDrawable.isCircular = true
+                    binding.userprofileImage.setImageDrawable(circularBitmapDrawable)
+                }
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
-
-    private fun updateAboutMe(aboutMe: String, currentUserUid: String) {
+    private fun updateAboutMe(aboutMe: String, currentUserUid: String){
         mDbRef.child("user").child(currentUserUid).child("aboutMe").setValue(aboutMe)
     }
-
     override fun onBackPressed() {
         super.onBackPressed()
         val intent = Intent(this@UserProfileScreen, MainActivity::class.java)
         finish()
         startActivity(intent)
-    }
-
-    private fun showPhoneNumberDialog(context: Context, listener: (String) -> Unit) {
-        val dialogView = LayoutInflater.from(context).inflate(R.layout.custom_dialog_phone_layout, null)
-        val editText = dialogView.findViewById<EditText>(R.id.edit_phone_text)
-
-        val dialog = AlertDialog.Builder(context)
-            .setView(dialogView)
-            .setTitle("Edit Phone Number")
-            .setPositiveButton("Save") { _, _ ->
-                val newPhoneNumber = editText.text.toString()
-                listener.invoke(newPhoneNumber)
-            }
-            .setNegativeButton("Cancel") { dialog, _ ->
-                dialog.cancel()
-            }
-            .create()
-
-        dialog.show()
     }
 }
